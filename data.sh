e61283e269c082e542adcdaaf6c1930df1d51b4d
Alex <alex@CT127.local>
2026-03-03 18:38:13 +0000

    use data generator to generate data

--- data.sh
+++ data.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-data=(
-  "Lana Merrick,42 Pinecone Ridge,58"
-  "Rufus Calder,19 Elmstone Drive,102"
-  "Maya Trenton,803 Silver Sparrow Way,77"
-  "Julian Brock,5 Old Quarry Road,63"
-  "Tessa Rylin,221 Maple Crest Lane,44"
-  "Cormac Vail,912 Brookhollow Court,89"
-  "Nina Halberg,37 Copper Lantern St,51"
-  "Elias Morren,1448 Whisperwind Trail,72"
-  "Ruby Fenwick,66 Harborview Terrace,95"
-  "Arden Kessler,303 Wildflower Bend,38"
-  "Sylvie Arden,10 Cedarbrook Path,84"
-  "Gavin Thorn,724 Redwood Crossing,57"
-);
-
-for row in "${data[@]}"
+curl -sS https://data-generator.alexk8s.com/v1/template/4f33666e-0996-4678-b9d6-ebb4fc6f6ba5/generate?count=20 | jq -r '.[]' | while IFS= read -r row;
 do
   ./bin/cli 127.0.0.1 "$row"
 done
+
+./bin/cli 127.0.0.1
